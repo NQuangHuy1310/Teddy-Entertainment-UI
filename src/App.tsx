c864b9b8bb587eb46577b1e92aef8143b800461c
Nguyen Quang Huy <kaisa13102004@gmail.com>
2024-03-25 11:42:49 +0700

    config/config-project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-center text-2xl">Hello world form react</h1>
+      <h1 className="">Hello world from react app</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ['selector', '[data-mode="dark"]'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      base: ['16px', '24px'],
+      lg: ['18px', '27px'],
+      xl: ['20px', '28px'],
+      '2xl': ['40px', '48px'],
+      '3xl': ['48px', '57px'],
+      '4xl': ['56px', '68px']
+    },
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif']
+      },
+      colors: {
+        primary: '#06aafe',
+        error: '#FF6370',
+        warning: '#ffc107',
+        success: '#58C27D',
+        secondary1: '#3C72A0',
+        secondary2: '#D6F4FF',
+        secondary3: '#6D3B00',
+
+        darkPrimary: '#191a24',
+        errorPriamry: '#FFB7A4'
+      },
+      backgroundColor: {
+        bgBase: '#fff',
+        bgDark: '#121212'
+      },
+      textColor: {
+        black: '#333'
+      },
+      screens: {
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1440px'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
