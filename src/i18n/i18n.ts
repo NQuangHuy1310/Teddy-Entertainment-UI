1e23f9103de5401ad1793f89b4f07db9c5da5738
Nguyen Quang Huy <kaisa13102004@gmail.com>
2024-04-01 09:39:08 +0700

    config/config-project-taildind+ladnguage

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,31 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import HOME_EN from '~/locales/en/home.json'
+import HOME_VI from '~/locales/vi/home.json'
+
+export const locales = {
+  en: 'English',
+  vi: 'Tiếng Việt'
+} as const
+
+export const resources = {
+  en: {
+    home: HOME_EN
+  },
+  vi: {
+    home: HOME_VI
+  }
+}
+
+export const defaultNS = 'home'
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'vi',
+  ns: ['home'],
+  defaultNS,
+  fallbackLng: 'vi',
+  interpolation: {
+    escapeValue: false
+  }
+})
